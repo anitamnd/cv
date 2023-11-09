6bec5adfe98023438012db3d054dde284825ccee
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2023-11-09 16:43:48 +0100

    update ui

--- ui.R
+++ ui.R
@@ -14,8 +14,11 @@ fluidPage(
       ),
       
       
-      p("Odense, Denmark"),
+      p("Denmark"),
       
+      a("Github", href = "https://github.com/anitamnd"),
+      a("LinkedIn", href = "https://www.linkedin.com/in/anitamnd/"),
+      br(),
       a("anamendesml@outlook.com", href = "mailto:anamendesml@outlook.com"),
       a("+45 55 22 20 45", href = "tel:+4555222045"),
       
@@ -37,7 +40,7 @@ fluidPage(
         tags$li("Team player"),
         tags$li("Fast learner"),
         tags$li("Organized"),
-        tags$li("?")
+        tags$li("Curious")
       ),
       
       h4("Skills"),
@@ -46,7 +49,8 @@ fluidPage(
         tags$li("Python"),
         tags$li("HTML"),
         tags$li("CSS"),
-        tags$li("")
+        tags$li("SQL"),
+        tags$li("Machine learning")
       ),
       
       p("References uppon request.")
@@ -55,46 +59,43 @@ fluidPage(
     mainPanel(
       fluidRow(h3("Relevant Experience"),
                div(
-                 column(7, h4("Student worker")),
-                 column(5, h5("2019 - Present")),
-                 column(12, p(
-                   "Protein Research Group, University of Southern Denmark"
-                 ))
-                 # Text mining, database curation
-               )),
-      fluidRow(
-        h3("Education"),
-        div(column(7, h5("Bioinformatics")),
+                 column(7, h4("Student assistant - Database curation and automation")),
+                 column(5, h5("2023 - Present")),
+                 column(12, 
+                        p("Protein Research Group, University of Southern Denmark"),
+                        tags$ul(
+                          tags$li("Python"),
+                          tags$li("Workflow automation"),
+                          tags$li("Clustering algorithms, Natural language processing, Predictive modeling"))
+                        )
+                 )
+              ),
+      fluidRow(h3("Education"),
+        div(column(7, h5("MSc. Computational Biomedicine")),
             column(5, h5("2022 - 2024")),
-            column(12, p(
-              "University of Southern Denmark"
-            ))),
-        div(column(7, h5(
-          "Biomedical Engineering"
-        )),
+            column(12, 
+                   p("University of Southern Denmark"),
+                   tags$ul(
+                     tags$li("'Computational Tools to Assess Substrate Flux and Fuel Preferences by In Vivo Tracer Analyses'"),
+                     tags$li("Shiny app development, Machine learning using caret package in R, fluxomics, metabolomics..."))
+                   )
+            ),
+        div(column(7, h5("BEng. Biomedical Engineering")),
         column(5, h5("2018 - 2021")),
-        column(
-          12,
+        column(12,
           p("Instituto Superior de Engenharia do Porto"),
           tags$ul(
-            tags$li(
-              "Interdisciplinary project: ADILUZ - Hand Hygiene Monitoring System:"
-            ),
+            tags$li("Interdisciplinary project: ADILUZ - Hand Hygiene Monitoring System:"),
             tags$li("Prototype, web-development, machine learning, deep learning")
           )
         )),
-        div(column(7, h5(
-          "European Project Semester"
-        )),
+        div(column(7, h5("European Project Semester")),
         column(5, h5("2018 - 2021")),
-        column(
-          12,
-          p("Instituto Superior de Engenharia do Porto"),
-          p("One semester project-based learning capstone program.")
+        column(12,
+          p("Multicultural and multidisciplinary semester with a project-based learning capstone program.")
         ))
       ),
-      fluidRow(
-        h3(""),
+      fluidRow(h3(""),
         h5("Vice-president, ESN Odense"),
         h5("Volunteer, Elderlearn"),
         h5("Hobbies: bouldering, crochet, reading")
